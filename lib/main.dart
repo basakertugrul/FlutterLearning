69652d20448d7459c0d766f32f2eec130d89cf71
basakertugrul <61563492+basakertugrul@users.noreply.github.com>
2020-08-18 21:18:13 +0300

    aciklama

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text("The question"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: () => print("Answer 1 choosen"),
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () => print("Answer 2 choosen"),
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: () => print("Answer 3 choosen"),
+            ),
+            RaisedButton(
+              child: Text("Answer 4"),
+              onPressed: () => print("Answer 4 choosen"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
